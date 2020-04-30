99fbc683f5004de44a84fbccf528e7d8ff100f1c
雪之萤 <44340666+HoTaru-Yuki@users.noreply.github.com>
2020-04-30 14:30:36 +0800

    Add files via upload

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -49,7 +49,7 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(ai_settings,screen,ship,aliens,bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
@@ -58,6 +58,16 @@ def update_bullets(bullets):
     for bullet in bullets.copy() :
         if bullet.rect.bottom <= 0 :
             bullets.remove(bullet)
+
+    # 检查是否有子弹击中了外星人
+    # 如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
+
+    if len(aliens) == 0:
+        # 删除现有的子弹并新建一群外星人
+        bullets.empty()
+        create_fleet(ai_settings,screen,ship,aliens)
+
 
 def fire_bullet(ai_settings,screen,ship,bullets):
     """如果还没有达到限制，就发射一颗子弹"""
@@ -104,4 +114,22 @@ def create_fleet(ai_settings,screen,ship,aliens):
 
 def update_aliens(aliens):
     """更新外星人群中所有外星人的位置"""
+    aliens.update()
+
+def check_fleet_edges(ai_settings,aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites() : 
+        if alien.check_edges() :
+           change_fleet_direction(ai_settings,aliens)
+           break
+
+def change_fleet_direction(ai_settings,aliens):
+    """将整群外星人下移，并改变他们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def update_aliens(ai_settings,aliens):
+    """检查是否有外星人位于屏幕边缘，并更新整群外星人的位置"""
+    check_fleet_edges(ai_settings,aliens)
     aliens.update()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -10,13 +10,16 @@ class Settings():
         # 飞船的设置
         self.ship_speed_factor = 1.5
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0,191,255)
         self.bullets_allowed = 3
         # 外星人设置
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
 
 
 
